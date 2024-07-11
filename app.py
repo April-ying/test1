d4ce7e8de6be7f3f7fe982accc1bad4965d08e2c
April <april910909@gmail.com>
2024-07-11 16:18:27 +0800

    後端刪掉註解，試試看能不能正常跑再繼續修改

--- app.py
+++ app.py
@@ -4,29 +4,9 @@ from flask_socketio import SocketIO, send,emit
 import base64
 from PIL import Image
 from io import BytesIO
-# 引入 Flask 和 PyMongo 套件
-# from flask_pymongo import PyMongo
-# from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-# 設定 MongoDB 連線資訊
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/dorm"
-# mongo = PyMongo(app)
-# socketio = SocketIO(app)
-
-# 建立 HTTPBasicAuth 物件
-# auth = HTTPBasicAuth()
-
-# 設定帳號密碼驗證機制
-# @auth.verify_password
-# def verify_password(username, password):
-#     return username == "admin" and password == "123456"
-
-# 定義路由和 HTTP 方法，並使用 auth.login_required decorator要求驗證
-# @app.route("/visitors", methods=["GET", "POST"])
-# @auth.login_required
-# def visitors():
 
 @app.route('/')
 def index():
@@ -68,19 +48,6 @@ def handwrite():
 def show():
     return render_template('show.html')
 
-# @app.route('/show_image')
-# def image():
-#     return render_template('show_image.html')
-
-# # 在 /receive_coordinates 路由上監聽來自客戶端的 POST 請求
-# @app.route('/receive_coordinates', methods=['POST'])
-# def receive_coordinates():
-#     data = request.get_json()   # 從 POST 請求中取得 JSON 格式的資料
-#     print("Received coordinates:", data)
-#     # 返回一個 JSON 格式的回應，其中包含一個名為 message 的屬性，指示座標資料已成功接收
-#     return jsonify({'message': 'Coordinates received successfully'})
-
-
 @app.route('/upload', methods=['POST'])
 def upload_image():
     data = request.json  # 從 POST 請求中獲取 JSON 格式的數據(前端傳來的圖片資料)
@@ -98,29 +65,6 @@ def upload_image():
     socketio.emit('image_uploaded', {'url': '/show'})
     return jsonify({'message': 'Image uploaded successfully'})
 
-# @socketio.on('button_press')
-# def handle_button_press(msg):
-#     print('Button Pressed:', msg)
-#     socketio.emit('button_press', msg)
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('start_drawing')
-def handle_start_drawing():
-    emit('start_drawing', broadcast=True)
-
-@socketio.on('submit_drawing')
-def handle_submit_drawing(image_data):
-    emit('display_drawing', image_data, broadcast=True)
-
 if __name__ == '__main__':
-    # socketio.run(app)
-    # socketio.run(app, debug=True,host='0.0.0.0', port=5000)
-    # app.run(debug=True)
     app.run(host='0.0.0.0', port=5000, debug=True)  #host改為0.0.0.0,讓手機暫時可以連上
-    # app.run(host='0.0.0.0') 
+    
